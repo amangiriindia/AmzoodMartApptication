2a32dc7761708239fa0295646216ffb58ddb3c01
Aman <amangiri381@gmail.com>
2023-04-24 12:25:19 +0530

    MainActivity work complete

--- app/src/main/java/com/company/amzoodmart/MainActivity.java
+++ app/src/main/java/com/company/amzoodmart/MainActivity.java
@@ -1,5 +1,7 @@
 package com.company.amzoodmart;
 
+import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SystemClock.sleep(3000);
+        Intent i =new Intent(MainActivity.this,loginActivity.class);
+        startActivity(i);
+        finish();
     }
 }
