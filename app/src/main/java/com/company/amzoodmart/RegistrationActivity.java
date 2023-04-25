51d4bec559e927ea1d3d2be712200b748c44dfab
Aman <amangiri381@gmail.com>
2023-04-25 14:01:42 +0530

    login and registion page desgin complete

--- app/src/main/java/com/company/amzoodmart/RegistrationActivity.java
+++ app/src/main/java/com/company/amzoodmart/RegistrationActivity.java
@@ -5,20 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class loginActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
     }
 
-
-    public void signin(View view){
-        startActivity(new Intent(loginActivity.this,MainActivity2.class));
-    }
     public void signup(View view){
-        startActivity(new Intent(loginActivity.this,RegistrationActivity.class));
+        startActivity(new Intent(RegistrationActivity.this,MainActivity2.class));
+    }
+    public void signin(View view){
+        startActivity(new Intent(RegistrationActivity.this,loginActivity.class));
     }
 
 }
